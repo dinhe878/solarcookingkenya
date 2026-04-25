fc2314a098a1279f10d658ffea2e1484088c3241
Ding He <dinghe6723@gmail.com>
2026-04-25 14:49:38 +0200

    update: replace some background photos

--- src/app/(public)/solarcookers/types/page.tsx
+++ src/app/(public)/solarcookers/types/page.tsx
@@ -12,7 +12,7 @@ export default function TypesOfCookers() {
       <div
         className="relative bg-accent text-accent-foreground"
         style={{
-          backgroundImage: `url('/Kakuma_camp_solar_cooking_solarcookers_dot_org_short.png')`,
+          backgroundImage: `url('/solarcooker.jpg')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}>
